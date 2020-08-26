3b5b3021f9a1e037ed8229d06bdf944bd4f6a27a
Thiru <Thiru@privacera.com>
2020-08-26 01:04:47 +0100

    mongodb changes

--- src/main/java/com/aicloudpods/SparkMinIO.java
+++ src/main/java/com/aicloudpods/SparkMinIO.java
@@ -1,20 +1,23 @@
 package com.aicloudpods;
 
-import org.apache.commons.io.FileUtils;
+import com.mongodb.spark.MongoSpark;
+import com.mongodb.spark.rdd.api.java.JavaMongoRDD;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.FileUtil;
-import org.apache.hadoop.fs.LocalFileSystem;
-import org.apache.hadoop.fs.Path;
 import org.apache.spark.SparkConf;
 import org.apache.spark.SparkContext;
-import org.apache.spark.sql.DataFrameReader;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.types.*;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
+import org.bson.Document;
+import scala.Function1;
 
-import java.net.URI;
+import java.util.Arrays;
 
 public class SparkMinIO {
 
@@ -32,16 +35,22 @@ public class SparkMinIO {
     });
 
     enum Env{
-        local("http://localhost:9000"),
-        cluster("http://minio.minio.svc.cluster.local:9000");
+        local("http://localhost:9000","127.0.0.1"),
+        cluster("http://minio.minio.svc.cluster.local:9000","mongodb.mongodb.svc.cluster.local");
         private String url;
+        private String mongoUrl;
 
-        Env(String url) {
+        Env(String url,String mongoUrl) {
             this.url = url;
+            this.mongoUrl = mongoUrl;
         }
 
         public String getUrl() {
             return url;
+        }
+
+        public String getMongoUrl() {
+            return mongoUrl;
         }
     }
 
@@ -59,18 +68,20 @@ public class SparkMinIO {
         Env env = Env.valueOf(args[0]);
 
         SparkConf conf = getSparkConfig(env);
+        conf.set("spark.mongodb.output.uri", "mongodb://root:fO1UGfRWwO@"+env.mongoUrl+":27017/aicp."+args[1]+"?authSource=admin");
         SparkContext sparkContext = new SparkContext(conf);
-        sparkContext.setLogLevel("WARN");
-        SparkSession spark = SparkSession.builder().sparkContext(sparkContext).getOrCreate();
+        //sparkContext.setLogLevel("WARN");
+        SparkSession spark = SparkSession
+                .builder()
+                .sparkContext(sparkContext)
+                .getOrCreate();
         Configuration configuration = sparkContext.hadoopConfiguration();
         configuration.set("fs.s3a.endpoint", env.url);
         configuration.set("fs.s3a.access.key", "AKIAIOSFODNN7EXAMPLE");
         configuration.set("fs.s3a.secret.key", "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY");
         configuration.set("fs.s3a.path.style.access", "true");
         configuration.set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem");
-//        LocalFileSystem localFileSystem = FileSystem.getLocal(new Configuration());
 
-        FileSystem fileSystem = FileSystem.get(new URI("s3a://aicp-minio/"),configuration);
         Dataset<Row> customers = spark.read()
                 .format("minioSelectCSV")
                 .schema(customerSchema)
@@ -79,9 +90,6 @@ public class SparkMinIO {
         Dataset<Row> txn = spark.read()
                 .format("minioSelectCSV")
                 .schema(txnSchema).load("s3://aicp-minio/txn.csv");
-       // customers.show();
-
-        //txn.show();
 
         Dataset <Row> joined = customers
                 .alias("c")
@@ -93,38 +101,44 @@ public class SparkMinIO {
                 joined
                         .groupBy("name")
                         .sum("amount")
-                        .orderBy("name")
-                        .write()
-                        .csv("name.csv");
-
-                FileUtil.copyMerge(fileSystem,new Path("name.csv"),fileSystem,new Path("name1.csv"),true, configuration, null);
+                        .orderBy("name");
                 break;
             }
             case nameaccno:{
                 joined
                         .groupBy("name","c.accno")
                         .sum("amount")
-                        .orderBy("name")
-                        .write()
-                        .csv("nameaccno.csv");
+                        .orderBy("name");
                 break;
             }
             case namemonth:{
                 joined
                         .groupBy("name","mon")
                         .sum("amount")
-                        .orderBy("name","mon")
-                        .write()
-                .csv("namemonth.csv");
+                        .orderBy("name","mon");
             }
         }
+
+        JavaSparkContext jsc = new JavaSparkContext(sparkContext);
+
+        JavaRDD<Document> documents = joined.toJSON().toJavaRDD().map(new Function<String, Document>() {
+            public Document call(final String json) throws Exception {
+                return Document.parse(json);
+            }
+        });
+
+        MongoSpark.save(documents);
+
+        JavaRDD<Document> fromMongo = MongoSpark.load(jsc);
+        ((JavaMongoRDD<Document>) fromMongo).toDF().show(1000);
 
         spark.stop();
         sparkContext.stop();
     }
 
     private static SparkConf getSparkConfig(Env env) {
-        SparkConf sparkConf = new SparkConf().setAppName("SparkMinIO");
+        SparkConf sparkConf = new SparkConf()
+                .setAppName("SparkMinIO");
         if(Env.local.equals(env)){
             sparkConf.setMaster("local[1]");
         }
